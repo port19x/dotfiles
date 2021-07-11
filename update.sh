6b8051c287720946138c67c145b95409b8107701
port19 <port19@port19.xyz>
2021-07-12 00:47:37 +0200

    updated update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-cp ~/.config/qutebrowser/config.py .
-cp ~/.config/ranger/rc.conf .
-
+cp ~/.config/qutebrowser/config.py ./qutebrowser
+cp ~/.config/ranger/rc.conf ./ranger
+cp ~/.config/kitty/kitty.conf ./kitty
+cp ~/.vimrc ./vim
+cp ~/.bashrc ./bash
