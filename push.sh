d7da5dfd7911b9521ebf7d4694e738457603e2d3
port19 <port19@port19.xyz>
2021-07-16 21:22:22 +0200

    extended and cleaned push.sh

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-cp  ./qutebrowser/config.py ~/.config/qutebrowser/
-cp  ./ranger/rc.conf ~/.config/ranger/
-cp  ./kitty/kitty.conf ~/.config/kitty/
-cp  ./vim/.vimrc ~/.vimrc
-cp  ./bash/.bashrc ~/
-cp  ./sway/config ~/.config/sway/
+cp  qutebrowser/config.py ~/.config/qutebrowser
+cp  ranger/rc.conf ~/.config/ranger
+cp  kitty/kitty.conf ~/.config/kitty
+cp  vim/.vimrc ~/.vimrc
+cp  bash/.bashrc ~/
+cp  sway/config ~/.config/sway
+cp  sway/colorscheme ~/.config/sway
