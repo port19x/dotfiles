9fbe81b9ef6a99be820b12ba31710145902fea91
port19 <port19@port19.xyz>
2021-09-04 02:00:19 +0200

    seperated colors from qutebrowser config

--- dots/.config/qutebrowser/config.py
+++ dots/.config/qutebrowser/config.py
@@ -62,28 +62,7 @@ c.fonts.prompts = 'default_size default_family'
 c.fonts.statusbar = 'default_size default_family'
 c.fonts.tabs.unselected = 'default_size default_family'
 
-# base16-qutebrowser (https://github.com/theova/base16-qutebrowser)
-# Base16 qutebrowser template by theova
-# Solarized Dark scheme by Ethan Schoonover (modified by aramisgithub)
-
-base00 = "#002b36"
-base01 = "#073642"
-base02 = "#586e75"
-base03 = "#657b83"
-base04 = "#839496"
-base05 = "#93a1a1"
-base06 = "#eee8d5"
-base07 = "#fdf6e3"
-base08 = "#dc322f"
-base09 = "#cb4b16"
-base0A = "#b58900"
-base0B = "#859900"
-base0C = "#2aa198"
-base0D = "#268bd2"
-base0E = "#6c71c4"
-base0F = "#d33682"
-
-# set qutebrowser colors
+from colors import *
 
 # Text color of the completion widget. May be a single color to use for
 # all columns or a list of three colors, one for each column.
