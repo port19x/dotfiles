808da81a6e5cd828238f844c06b27fe3bec5f70a
port19 <port19@port19.xyz>
2021-10-21 11:12:27 +0200

    more appropiate keybindings

--- dots/.config/qtile/config.py
+++ dots/.config/qtile/config.py
@@ -1,63 +1,38 @@
 from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.config import Group, Key, Match, Screen
 from libqtile.lazy import lazy
 
 mod = "mod4"
 terminal = "kitty"
 
-base00 = "#002b36"
-base01 = "#073642"
-base05 = "#93a1a1"
-base0D = "#268bd2"
+base0 = "#002b36"
+base1 = "#073642"
+base5 = "#93a1a1"
+baseD = "#268bd2"
 
 keys = [
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),
-        desc="Move window focus to other window"),
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "l", lazy.layout.right()),
+    Key([mod], "j", lazy.layout.down()),
+    Key([mod], "k", lazy.layout.up()),
+    Key([mod], "space", lazy.layout.next()),
+    Key([mod, "shift"], "h", lazy.layout.swap_left()),
+    Key([mod, "shift"], "l", lazy.layout.swap_right()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+    Key([mod], "i", lazy.layout.grow()),
+    Key([mod], "m", lazy.layout.shrink()),
+    Key([mod], "n", lazy.layout.normalize()),
+    Key([mod], "o", lazy.layout.maximize()),
+    Key([mod, "shift"], "space", lazy.layout.flip()),
 
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
+    Key([mod], "Tab", lazy.next_layout()),
+    Key([mod], "w", lazy.window.kill()),
+    Key([mod, "control"], "r", lazy.restart()),
+    Key([mod, "control"], "q", lazy.shutdown()),
+    Key([mod], "r", lazy.spawncmd()),
 ]
 
 groups = [Group(i) for i in "123"]
@@ -73,16 +48,16 @@ for i in groups:
 
 layouts = [
     layout.Max(),
-    layout.MonadTall(border_width=1, border_focus=base0D, border_normal=base00),
-    layout.MonadWide(border_width=1, border_focus=base0D, border_normal=base00),
+    layout.MonadTall(border_width=1, border_focus=baseD, border_normal=base0),
+    layout.MonadWide(border_width=1, border_focus=baseD, border_normal=base0),
 ]
 
 widget_defaults = dict(
     font='mononoki',
     fontsize=14,
     padding=10,
-    background=base00,
-    foreground=base05,
+    background=base0,
+    foreground=base5,
 )
 extension_defaults = widget_defaults.copy()
 
@@ -92,26 +67,26 @@ screens = [
         wallpaper="~/Pictures/base00.png",
         top=bar.Bar(
             [
-                widget.GroupBox(active=base05, inactive=base05, padding=0),
+                widget.GroupBox(active=base5, inactive=base5, padding=0),
                 widget.Prompt(),
                 widget.WindowName(),
                 widget.Spacer(),
                 widget.CurrentLayout(fmt="{} "),
-#                widget.Wttr(format="?T%t", location="redacted"),
+                # widget.Wttr(format="?T%t", location="redacted"),
                 widget.Net(fmt="{} ", format="{down}"),
                 widget.Net(fmt="{} ", format="{up}"),
-#                widget.CryptoTicker(fmt="{} ", crypto="XMR", currency="EUR"),
-#                widget.PulseVolume(fmt="{} "),
+                # widget.CryptoTicker(fmt="{} ", crypto="XMR"),
+                # widget.PulseVolume(fmt="{} "),
                 widget.CPU(fmt="{}% ", format="{load_percent}"),
                 widget.Memory(fmt="{}% ", format="{MemPercent}"),
-                widget.DF(fmt="{} ", format="{r:.0f}%", visible_on_warn = False),
-                widget.ThermalSensor(fmt="{} ", foreground=base05),
-#                widget.Backlight(fmt="{} ", format="{percent:1%}"),
-#                widget.Battery(fmt="{} ", format="{percent:1%}"),
+                widget.DF(fmt="{} ", format="{r:.0f}%", visible_on_warn=False),
+                widget.ThermalSensor(fmt="{} ", foreground=base5),
+                # widget.Backlight(fmt="{} ", format="{percent:1%}"),
+                # widget.Battery(fmt="{} ", format="{percent:1%}"),
                 widget.CheckUpdates(distro="Debian", fmt="{} ", display_format="{updates}",\
- no_update_string="0", colour_have_updates=base05, colour_no_updates=base05),
-#                widget.ImapWidget(fmt="{} "),
-#                widget.KhalCalendar(fmt="{} "),
+ no_update_string="0", colour_have_updates=base5, colour_no_updates=base5),
+                # widget.ImapWidget(fmt="{} "),
+                # widget.KhalCalendar(fmt="{} "),
                 widget.Clock(fmt="{} ", format='%a-%d.%m.%y'),
                 widget.Clock(fmt="{} ", format='%H:%M'),
             ],
@@ -119,15 +94,6 @@ screens = [
         ),
     ),
     Screen(),
-]
-
-# Drag floating layouts.
-mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
