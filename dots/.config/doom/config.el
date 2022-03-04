771ea2814724c38ff8c2d3b1a59bf0de28aa093e
port19 <port19@port19.xyz>
2022-03-04 22:47:26 +0100

    add decomented doom config

---
+++ dots/.config/doom/config.el
@@ -0,0 +1,11 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;; If you just installed doom emacs on a new distro, remember to tweak init.el and packages.el
+(setq user-full-name "ura43"
+      user-mail-address "ura@ura43.xyz")
+(setq doom-font (font-spec :family "Mononoki" :size 32)
+      doom-big-font (font-spec :family "Mononoki" :size 64))
+(setq display-line-numbers-type 'relative)
+(setq org-directory "~/Documents/")
+;; - `map!' for binding new keys (press K on it for docs)
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
