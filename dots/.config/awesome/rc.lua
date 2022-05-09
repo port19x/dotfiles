70c83c85f82cadf8aa5252dac3ca828a23430beb
port19 <port19@port19.xyz>
2022-05-09 10:29:37 +0200

    awesome: remove mouse bindings

--- dots/.config/awesome/rc.lua
+++ dots/.config/awesome/rc.lua
@@ -145,14 +145,6 @@ awful.screen.connect_for_each_screen(function(s)
 end)
 -- }}}
 
--- {{{ Mouse bindings
-root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
--- }}}
-
 -- {{{ Key bindings
 globalkeys = gears.table.join(
     awful.key({ modkey,           }, "k",      hotkeys_popup.show_help,
