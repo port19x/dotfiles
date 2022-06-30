e6a2719eb543cd8cbc2fae9ebf3cf0e9d2ae9c1e
port19 <port19@port19.xyz>
2022-06-30 08:46:12 +0200

    awesome: always awful.spawn.with_shell

--- dots/.config/awesome/rc.lua
+++ dots/.config/awesome/rc.lua
@@ -252,10 +252,10 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- {{{ Autostart
 -- awful.spawn.with_shell("find ~/pic/Wallpapers/ | shuf -n 1 | xargs xwallpaper --center")
-awful.spawn("xwallpaper --center ~/pic/Wallpapers/purple-light.jpg")
+awful.spawn.with_shell("xwallpaper --center ~/pic/Wallpapers/purple-light.jpg")
 awful.spawn.with_shell("command -v qutebrowser && pgrep -x qutebrowser || qutebrowser")
 awful.spawn.with_shell("command -v kitty && pgrep -x kitty || kitty")
 awful.spawn.with_shell("command -v emacs && pgrep -x emacs || emacs --daemon")
 awful.spawn.with_shell("command -v picom && pgrep -x picom || picom --daemon")
-awful.spawn("setxkbmap de")
+awful.spawn.with_shell("setxkbmap de")
 --- }}}
