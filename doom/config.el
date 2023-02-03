a25f113e9152447ec949a223abbe180c079b82b8
port19 <port19@port19.xyz>
2023-02-03 12:04:24 +0100

    emacs: dired & launcher improvements

--- doom/config.el
+++ doom/config.el
@@ -6,10 +6,16 @@
       user-full-name "port19"
       user-mail-address "port19@port19.xyz"
       display-line-numbers-type `relative
-      org-directory "~/doc/")
+      org-directory "~/doc/"
+      dired-kill-when-opening-new-dired-buffer t)
 
 (add-hook 'org-mode-hook (lambda () (delete '("\\.pdf\\'" . default) org-file-apps)
                                     (add-to-list 'org-file-apps '("\\.pdf\\'" . "mupdf %s"))))
+
+(add-to-list 'display-buffer-alist
+                 '(;; no window
+                   "\\`\\*Async Shell Command\\*\\'"
+                   (display-buffer-no-window)))
 
 (map! :after cider-mode :map clojure-mode-map :n "," #'cider-eval-last-sexp)
 (map! :after cider-mode :map clojure-mode-map :n ";" #'cider-format-buffer)
