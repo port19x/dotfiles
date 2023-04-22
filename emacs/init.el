f273eafc03086c1643177a44a9fad99adfe1d1f5
port19 <port19@port19.xyz>
2023-04-23 00:15:42 +0200

    emacs: even more uniform indentation

--- emacs/init.el
+++ emacs/init.el
@@ -26,55 +26,54 @@
 (setq evil-want-keybinding nil)
 
 ;;; dashboard ;;;
-(use-package dashboard
-  :preface (defun my/dashboard-banner ()
-             (setq dashbard-banner-logo-title
-                   (format "Emacs ready in %s seconds with %d garbage collections."
-                           (emacs-init-time) gcs-done)))
-  :custom (dashboard-startup-banner "~/dotfiles/emacs/avatar.gif")
-          (initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
-          (dashboard-center-content t)
-          (dashboard-items '((recents  . 5) (bookmarks . 5) (projects . 5)))
-  :config (dashboard-setup-startup-hook)
-  :hook   ((after-init     . dashboard-refresh-buffer)
-          (dashboard-mode . my/dashboard-banner)))
+(use-package dashboard           :preface (defun my/dashboard-banner ()
+                                            (setq dashbard-banner-logo-title
+                                                  (format "Emacs ready in %s seconds with %d garbage collections."
+                                                          (emacs-init-time) gcs-done)))
+                                 :custom  (dashboard-startup-banner "~/dotfiles/emacs/avatar.gif")
+                                          (initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+                                          (dashboard-center-content t)
+                                          (dashboard-items '((recents  . 5) (bookmarks . 5) (projects . 5)))
+                                 :config  (dashboard-setup-startup-hook)
+                                 :hook    ((after-init     . dashboard-refresh-buffer)
+                                           (dashboard-mode . my/dashboard-banner)))
 
 ;;; look and feel ;;;
 (set-face-attribute 'default nil :font "Iosevka" :height 140)
 (use-package better-defaults)
-(use-package doom-themes        :init   (load-theme 'doom-nord-aurora t))
-(use-package doom-modeline      :init   (doom-modeline-mode))
-(use-package hl-todo            :init   (global-hl-todo-mode))
-(use-package git-gutter         :init   (global-git-gutter-mode))
-(use-package org-superstar      :hook   (org-mode . org-superstar-mode))
+(use-package doom-themes         :init    (load-theme 'doom-nord-aurora t))
+(use-package doom-modeline       :init    (doom-modeline-mode))
+(use-package hl-todo             :init    (global-hl-todo-mode))
+(use-package git-gutter          :init    (global-git-gutter-mode))
+(use-package org-superstar       :hook    (org-mode . org-superstar-mode))
 
 ;;; completion ;;;
-(use-package vertico            :custom (vertico-resize t)
-                                :init   (vertico-mode))
-(use-package marginalia         :init   (marginalia-mode))
-(use-package which-key          :init   (which-key-mode))
-(use-package projectile         :init   (projectile-mode +1))
-(use-package corfu              :custom (corfu-auto t)
-                                :init   (global-corfu-mode))
+(use-package vertico             :custom  (vertico-resize t)
+                                 :init    (vertico-mode))
+(use-package marginalia          :init    (marginalia-mode))
+(use-package which-key           :init    (which-key-mode))
+(use-package projectile          :init    (projectile-mode +1))
+(use-package corfu               :custom  (corfu-auto t)
+                                 :init    (global-corfu-mode))
 
 ;;; evil keys ;;;
-(use-package evil               :init   (evil-mode 1)
-                                :custom (evil-undo-system 'undo-redo))
-(use-package evil-goggles       :init   (evil-goggles-mode))
-(use-package evil-vimish-fold   :init   (global-evil-vimish-fold-mode))
-(use-package evil-collection    :init   (evil-collection-init))
+(use-package evil                :init    (evil-mode 1)
+                                 :custom  (evil-undo-system 'undo-redo))
+(use-package evil-goggles        :init    (evil-goggles-mode))
+(use-package evil-vimish-fold    :init    (global-evil-vimish-fold-mode))
+(use-package evil-collection     :init    (evil-collection-init))
 
 ;;; clojure ;;;
 (use-package clojure-mode)
 (use-package cider)
-(use-package rainbow-delimiters :hook   (prog-mode . rainbow-delimiters-mode))
-(use-package smartparens        :hook   (prog-mode . smartparens-mode))
-(use-package format-all         :hook   (clojure-mode . format-all-mode))
-(use-package paredit            :hook   (clojure-mode . paredit-mode)
-                                :bind   (:map clojure-mode-map ("<" . paredit-forward-slurp-sexp)))
-(use-package lsp-mode           :hook   (clojure-mode . lsp))
-(use-package lsp-ui             :custom (lsp-ui-doc-show-with-cursor t)
-                                        (lsp-ui-doc-delay 2.0))
+(use-package rainbow-delimiters  :hook    (prog-mode . rainbow-delimiters-mode))
+(use-package smartparens         :hook    (prog-mode . smartparens-mode))
+(use-package format-all          :hook    (clojure-mode . format-all-mode))
+(use-package paredit             :hook    (clojure-mode . paredit-mode)
+                                 :bind    (:map clojure-mode-map ("<" . paredit-forward-slurp-sexp)))
+(use-package lsp-mode            :hook    (clojure-mode . lsp))
+(use-package lsp-ui              :custom  (lsp-ui-doc-show-with-cursor t)
+                                          (lsp-ui-doc-delay 2.0))
 
 ;;; other programming related modes ;;;
 (use-package magit)
