3d1fc58bd8ce0cc0948f2b21cdcb2040b7de3dfe
port19 <port19@port19.xyz>
2023-05-04 11:51:04 +0200

    emacs: some lazy-loading & drop /inline better-defaults package

--- emacs/init.el
+++ emacs/init.el
@@ -10,12 +10,20 @@
 (setq use-package-always-ensure t)
 
 ;;; better defaults ;;; 
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(horizontal-scroll-bar-mode -1)
+(savehist-mode 1)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (setq inhibit-startup-message t
       custom-file (concat user-emacs-directory "/custom.el")
       display-line-numbers-type `relative
       native-comp-deferred-compilation nil
-      dired-kill-when-opening-new-dired-buffer t)
+      dired-kill-when-opening-new-dired-buffer t
+      indent-tabs-mode nil
+      ubiquify-buffer-name-style 'forward
+      visible-bell t)
 
 ;;; personal settings ;;;
 (setq user-full-name "port19"
@@ -25,7 +33,7 @@
 ;;; needed before evil gets loaded ;;;
 (setq evil-want-keybinding nil)
 
-;;; dashboard ;;;
+;;; dashboard ;;; FIXME make dashboard display sooner (?)
 (use-package dashboard           :preface (defun my/dashboard-banner ()
                                             (setq dashbard-banner-logo-title
                                                   (format "Emacs ready in %s seconds with %d garbage collections."
@@ -40,12 +48,8 @@
 
 ;;; look and feel ;;;
 (set-face-attribute 'default nil :font "Iosevka" :height 140)
-(use-package better-defaults)
 (use-package doom-themes         :init    (load-theme 'doom-nord-aurora t))
 (use-package doom-modeline       :init    (doom-modeline-mode))
-(use-package hl-todo             :init    (global-hl-todo-mode))
-(use-package git-gutter          :init    (global-git-gutter-mode))
-(use-package org-superstar       :hook    (org-mode . org-superstar-mode))
 
 ;;; completion ;;;
 (use-package vertico             :custom  (vertico-resize t)
@@ -63,25 +67,37 @@
 (use-package evil-vimish-fold    :init    (global-evil-vimish-fold-mode))
 (use-package evil-collection     :init    (evil-collection-init))
 
-;;; clojure ;;;
-(use-package clojure-mode)
-(use-package cider)
+;;; clojure ;;; TODO from lsp to flycheck setup
+;;; TODO bind several cider keys to the cider keymap
+(use-package clojure-mode        :mode    "\\.edn\\'" "\\.clj?[scx]\\'")
+(use-package cider               :after   (clojure-mode))
 (use-package rainbow-delimiters  :hook    (prog-mode . rainbow-delimiters-mode))
 (use-package smartparens         :hook    (prog-mode . smartparens-mode))
 (use-package format-all          :hook    (clojure-mode . format-all-mode))
 (use-package paredit             :hook    (clojure-mode . paredit-mode)
                                  :bind    (:map clojure-mode-map ("<" . paredit-forward-slurp-sexp)))
 (use-package lsp-mode            :hook    (clojure-mode . lsp))
-(use-package lsp-ui              :custom  (lsp-ui-doc-show-with-cursor t)
+(use-package lsp-ui              :after   (lsp-mode)
+                                 :custom  (lsp-ui-doc-show-with-cursor t)
                                           (lsp-ui-doc-delay 2.0))
 
 ;;; other programming related modes ;;;
 (use-package magit)
-(use-package markdown-mode)
-(use-package lua-mode)
-(use-package ansible)
+(use-package hl-todo             :init    (global-hl-todo-mode))
+(use-package git-gutter          :init    (global-git-gutter-mode))
+(use-package markdown-mode       :mode    "\\.md\\'")
+(use-package lua-mode            :mode    "\\.lua\\'")
+(use-package ansible             :mode    "\\.ya?ml\\'")
 
 ;;; media related stuff ;;;
-(use-package org-re-reveal)
-(use-package pdf-tools)
-(use-package nov)
+(use-package org-re-reveal       :after   (org-mode))
+(use-package org-superstar       :hook    (org-mode . org-superstar-mode))
+(use-package pdf-tools           :magic   ("%PDF" . pdf-view-mode)
+                                 :config  (pdf-tools-install :no-query))
+(use-package nov                 :mode    "\\.epub\\'")
+
+;;; TODO: space based global binds:
+;(use-package helpful)
+;SPC-h-f helpful describe function
+;SPC-f-f find-file
+;SPC-b-b ibuffer
