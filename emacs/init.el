6580aea802ffd12e2951916ae23f7d3c4b2a3b28
port19 <port19@port19.xyz>
2023-06-08 13:43:34 +0200

    emacs: ordered config into meaningful foldable chunks

--- emacs/init.el
+++ emacs/init.el
@@ -38,7 +38,7 @@
   (after-init . (lambda () (setq gc-cons-threshold (* 8 1024 1024))))
   (after-init . elfeed-update))
 
-(use-package org-contrib
+(use-package org-superstar
   :config
   (org-babel-do-load-languages
    'org-babel-load-languages
@@ -52,7 +52,9 @@
   (org-default-notes-file "~/doc/org/notes.org")
   (org-agenda-files '("~/doc/org"))
   (org-capture-templates '(("c" "Coaching / Emacs todo" checkitem (file ""))))
-  (org-confirm-babel-evaluate nil))
+  (org-confirm-babel-evaluate nil)
+  :hook
+  (org-mode . org-superstar-mode))
 
 (use-package dashboard
   :custom
@@ -64,49 +66,34 @@
   (server-after-make-frame     . dashboard-refresh-buffer))
 
 (use-package elfeed
-  :custom
-  (elfeed-feeds '("https://port19.xyz/rss.xml"
-                  "https://mitchmarq42.xyz/index.xml"
-                  "https://lukesmith.xyz/index.xml"
-                  "https://feeds.transistor.fm/thoughts-on-functional-programming-podcast-by-eric-normand"
-                  "https://protesilaos.com/commentary.xml"
-                  "https://protesilaos.com/codelog.xml"
-                  "https://protesilaos.com/news.xml"
-                  "https://planet.archlinux.org/rss20.xml"
-                  "https://distrowatch.com/news/dwd.xml"
-                  "https://clojure.org/feed.xml"
-                  "https://github.blog/changelog/feed")))
+  :custom (elfeed-feeds '("https://port19.xyz/rss.xml" "https://mitchmarq42.xyz/index.xml" "https://lukesmith.xyz/index.xml"
+                          "https://feeds.transistor.fm/thoughts-on-functional-programming-podcast-by-eric-normand"
+                          "https://protesilaos.com/commentary.xml" "https://protesilaos.com/codelog.xml" "https://protesilaos.com/news.xml"
+                          "https://planet.archlinux.org/rss20.xml" "https://distrowatch.com/news/dwd.xml" "https://lwn.net/headlines/rss"
+                          "https://github.blog/changelog/feed" "https://sachachua.com/blog/category/emacs-news/feed/" "https://clojure.org/feed.xml" 
+                          "https://openrss.org/github.com/pystardust/ani-cli/issues" "https://openrss.org/github.com/pystardust/ani-cli/pulls")))
 
+; UI
 (use-package doom-themes        :config (load-theme 'doom-nord-aurora t))
 (use-package doom-modeline      :config (doom-modeline-mode)) ;nerd-icons-install-fonts
-(use-package helpful            :custom (helpful-max-buffers 3))
 (use-package beacon             :config (beacon-mode 1))
+(use-package helpful            :custom (helpful-max-buffers 3))
+(use-package which-key          :config (which-key-mode)
+                                :custom (which-key-sort-order 'which-key-key-order-alpha))
+(use-package eshell-toggle      :custom (eshell-history-size 100000))
 
+; Completion Stack
 (use-package vertico            :config (vertico-mode)
                                 :custom (vertico-resize t))
 (use-package orderless          :custom (completion-styles '(orderless basic))
                                         (orderless-matching-styles '(orderless-flex)))
 (use-package marginalia         :config (marginalia-mode))
-(use-package which-key          :config (which-key-mode)
-                                :custom (which-key-sort-order 'which-key-key-order-alpha))
+(use-package consult-projectile :config (projectile-mode +1))
 (use-package corfu              :custom (corfu-auto t)
                                 :config (global-corfu-mode))
-(use-package consult-projectile :config (projectile-mode +1))
-
-(use-package eshell-toggle      :custom (eshell-history-size 100000))
 (use-package esh-autosuggest    :hook (eshell-mode . esh-autosuggest-mode))
-(use-package vterm              :custom (vterm-always-compile-module t))
-(use-package pdf-tools          :magic  ("%PDF" . pdf-view-mode)
-                                :config (pdf-tools-install :no-query))
-(use-package org-superstar      :hook   (org-mode . org-superstar-mode))
 
-(use-package evil               :init   (setq evil-want-keybinding nil)
-                                :config (evil-mode 1)
-                                :custom (evil-undo-system 'undo-redo))
-(use-package evil-goggles       :config (evil-goggles-mode))
-(use-package evil-vimish-fold   :config (global-evil-vimish-fold-mode))
-(use-package evil-collection    :config (evil-collection-init))
-
+; Clojure
 (use-package clojure-mode       :mode   "\\.edn\\'" "\\.clj?[scx]\\'")
 (use-package cider              :custom (cider-repl-pop-to-buffer-on-connect . nil))
 (use-package clj-refactor       :custom (cljr-project-clean-prompt nil))
@@ -114,17 +101,26 @@
 (use-package paredit            :hook   (clojure-mode . paredit-mode))
 (use-package eglot              :hook   (clojure-mode . eglot-ensure))
 
+; Further Programming Modes
 (use-package magit              :custom (magit-slow-confirm nil))
-(use-package hl-todo            :config (global-hl-todo-mode))
 (use-package git-gutter         :config (global-git-gutter-mode))
+(use-package hl-todo            :config (global-hl-todo-mode))
+(use-package vterm              :custom (vterm-always-compile-module t))
 (use-package markdown-mode      :mode   "\\.md\\'")
 (use-package lua-mode           :mode   "\\.lua\\'")
 (use-package ansible            :mode   "\\.ya?ml\\'")
 
+; Key Bindigns
+(use-package evil               :init   (setq evil-want-keybinding nil)
+                                :config (evil-mode 1)
+                                :custom (evil-undo-system 'undo-redo))
+(use-package evil-goggles       :config (evil-goggles-mode))
+(use-package evil-vimish-fold   :config (global-evil-vimish-fold-mode))
+(use-package evil-collection    :config (evil-collection-init))
 (use-package general
   :config
   (general-evil-setup)
-  (general-create-definer my-leader-keys
+  (general-create-definer my-spc-map
     :keymaps '(normal visual insert emacs)
     :prefix "SPC"
     :global-prefix "C-M-SPC")
@@ -147,7 +143,7 @@
       (define-key map (kbd "i") #'ibuffer)
       (define-key map (kbd "k") #'kill-current-buffer)
       (define-key map (kbd "m") #'switch-to-buffer)
-      (define-key map (kbd "n") #'next-buffer) ;FIXME ignore * buffers
+      (define-key map (kbd "n") #'next-buffer)
       (define-key map (kbd "r") #'revert-buffer)
       map))
 
@@ -222,7 +218,7 @@
       (define-key map (kbd "x") #'org-export-dispatch)
       map))
 
-  (my-leader-keys
+  (my-spc-map
     "a" '(org-agenda :which-key "org agenda")
     "b" `(,my-buffer-map :which-key "Buffer")
     "c" '(magit-clone :which-key "magit clone")
@@ -253,7 +249,6 @@
     "v" '(eval-last-sexp :which-key "(emacs) eval")
     "w" `(,my-window-map :which-key "Windows")
     "x" '(org-capture :which-key "org capture")
-    ;y elpaca try
     "z" '(zone :which-key "zone")
     "SPC" `(,my-clojure-map :which-key "Clojure")
     "<return>" '(consult-bookmark :which-key "jump to bookmark")
