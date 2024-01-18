a675976a9f477d0d5324d7aedabc6844dcea71a7
port19 <port19@port19.xyz>
2024-01-18 13:59:19 +0100

    emacs: revert canonical use-package formatting
    This reverts commit 1449cc92ee22aec0e44655584cbd7b682f692e88.

--- init.el
+++ init.el
@@ -10,205 +10,116 @@
 (unless package-archive-contents (package-refresh-contents))
 (use-package no-littering)
 
-(use-package emacs
-  :config
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (horizontal-scroll-bar-mode -1)
-  (indent-tabs-mode -1)
-  (savehist-mode 1)
-  (save-place-mode 1)
-  (global-auto-revert-mode 1)
-  (display-time-mode 1)
-  :custom
-  (inhibit-startup-message t)
-  (visible-bell t)
-  (dired-kill-when-opening-new-dired-buffer t)
-  (make-backup-files nil)
-  (custom-file (concat user-emacs-directory "/custom.el"))
-  :hook
-  (prog-mode . electric-pair-mode)
-  (after-init . (lambda () (set-face-attribute 'default nil :font "Iosevka" :height 140)))
-  (after-init . (lambda () (setq gc-cons-threshold (* 8 1024 1024))))
-  (dired-mode . dired-hide-details-mode))
+(use-package emacs                :config (menu-bar-mode -1) (tool-bar-mode -1) (scroll-bar-mode -1) (horizontal-scroll-bar-mode -1) (indent-tabs-mode -1)
+                                          (savehist-mode 1) (save-place-mode 1) (global-auto-revert-mode 1) (display-time-mode 1)
+                                  :custom (inhibit-startup-message t)
+                                          (visible-bell t)
+                                          (dired-kill-when-opening-new-dired-buffer t)
+                                          (make-backup-files nil)
+                                          (custom-file (concat user-emacs-directory "/custom.el"))
+                                  :hook   (prog-mode . electric-pair-mode)
+                                          (after-init . (lambda () (set-face-attribute 'default nil :font "Iosevka" :height 140)))
+                                          (after-init . (lambda () (setq gc-cons-threshold (* 8 1024 1024))))
+					  (dired-mode . dired-hide-details-mode))
 
-(use-package org-modern
-  :config
-  (org-babel-do-load-languages 'org-babel-load-languages '((python . t) (shell . t)))
-  (require 'oc-biblatex)
-  :custom
-  (org-startup-indented t)
-  (org-edit-src-content-indentation 0)
-  (org-src-preserve-indentation t)
-  (org-confirm-babel-evaluate nil)
-  (org-agenda-files '("~/doc/master.org"))
-  (org-agenda-restore-windows-after-quit t)
-  (org-capture-templates '(("a" "Appointment" entry (file+headline "~/doc/master.org" "📅 Agenda") "** %t ")
-			   ("t" "Todo" entry (file+headline "~/doc/master.org" "📅 Agenda") "** TODO ")
-			   ("e" "Emacs/Linux Todo" item (file+headline "~/doc/master.org" "🪓 Emacs Todo"))
-			   ("i" "Article Idea" item (file+headline "~/doc/master.org" "📜 Article Ideas"))))
-  :hook org-mode)
+(use-package org-modern           :config (org-babel-do-load-languages 'org-babel-load-languages '((python . t) (shell . t)))
+                                          (require 'oc-biblatex)
+                                  :custom (org-startup-indented t)
+                                          (org-edit-src-content-indentation 0)
+                                          (org-src-preserve-indentation t)
+                                          (org-confirm-babel-evaluate nil)
+                                          (org-agenda-files '("~/doc/master.org"))
+                                          (org-agenda-restore-windows-after-quit t)
+                                          (org-capture-templates '(("a" "Appointment" entry (file+headline "~/doc/master.org" "📅 Agenda") "** %t ")
+								   ("t" "Todo" entry (file+headline "~/doc/master.org" "📅 Agenda") "** TODO ")
+								   ("e" "Emacs/Linux Todo" item (file+headline "~/doc/master.org" "🪓 Emacs Todo"))
+								   ("i" "Article Idea" item (file+headline "~/doc/master.org" "📜 Article Ideas"))))
+                                  :hook   org-mode)
 
-(use-package exwm
-  :init
-  (require 'exwm-randr)
-  (exwm-randr-enable)
-  (start-process-shell-command "xrandr" nil "xrandr --output DP-1-1 --right-of eDP --output DP-1-2 --right-of DP-1-1")
-  (start-process-shell-command "xrandr" nil "xrandr --output DP-1-1 --rotate left --output DP-1-2 --rotate right")
-  (exwm-enable)
-  :config
-  (add-to-list 'exwm-input-prefix-keys ?\M- )
-  :custom
-  (exwm-workspace-number 3)
-  (exwm-randr-workspace-monitor-plist '(0 "eDP" 1 "DP-1-1" 2 "DP-1-2"))
-  :hook
-  (exwm-update-class . (lambda () (exwm-workspace-rename-buffer exwm-class-name))))
+; UI & Help
+(use-package exwm                 :init   (require 'exwm-randr)
+                                          (exwm-randr-enable)
+					  (start-process-shell-command "xrandr" nil "xrandr --output DP-1-1 --right-of eDP --output DP-1-2 --right-of DP-1-1")
+					  (start-process-shell-command "xrandr" nil "xrandr --output DP-1-1 --rotate left --output DP-1-2 --rotate right")
+                                          (exwm-enable)
+                                  :config (add-to-list 'exwm-input-prefix-keys ?\M- )
+                                  :custom (exwm-workspace-number 3)
+				          (exwm-randr-workspace-monitor-plist '(0 "eDP" 1 "DP-1-1" 2 "DP-1-2"))
+                                  :hook   (exwm-update-class . (lambda () (exwm-workspace-rename-buffer exwm-class-name))))
+(use-package dashboard            :custom (dashboard-startup-banner "~/pic/dashboard.jpg")
+                                          (dashboard-center-content t)
+                                          (dashboard-items '((recents  . 5) (bookmarks . 5) (projects . 5)))
+                                  :hook   (after-init . dashboard-refresh-buffer))
+(use-package ef-themes            :config (load-theme 'ef-maris-dark t))
+(use-package doom-modeline        :config (doom-modeline-mode)) ;nerd-icons-install-fonts
+(use-package dired-filter         :init   (defun toggle-hide-dots () (interactive)
+                                               (if (= (length dired-filter-stack) 0) (dired-filter-by-dot-files) (dired-filter-pop-all)))
+                                  :hook   (dired-mode . dired-filter-by-dot-files)
+                                  :bind   (:map dired-mode-map ("," . toggle-hide-dots)))
+(use-package nerd-icons-dired     :hook   dired-mode)
+(use-package hl-todo              :config (global-hl-todo-mode))
+(use-package define-it            :custom (define-it-show-google-translate nil))
+(use-package helpful              :custom (helpful-max-buffers 3))
+(use-package marginalia           :config (marginalia-mode))
+(use-package which-key            :config (which-key-mode)
+                                  :custom (which-key-sort-order 'which-key-key-order-alpha))
 
-(use-package dashboard
-  :custom
-  (dashboard-startup-banner "~/pic/dashboard.jpg")
-  (dashboard-center-content t)
-  (dashboard-items '((recents  . 5) (bookmarks . 5) (projects . 5)))
-  :hook
-  (after-init . dashboard-refresh-buffer))
-
-(use-package ef-themes
-  :config (load-theme 'ef-maris-dark t))
-
-(use-package doom-modeline
-  :config (doom-modeline-mode)) ;nerd-icons-install-fonts
-
-(use-package dired-filter
-  :init
-  (defun toggle-hide-dots () (interactive)
-         (if (= (length dired-filter-stack) 0)
-	     (dired-filter-by-dot-files)
-	   (dired-filter-pop-all)))
-  :hook (dired-mode . dired-filter-by-dot-files)
-  :bind (:map dired-mode-map ("," . toggle-hide-dots)))
-
-(use-package nerd-icons-dired
-  :hook   dired-mode)
-
-(use-package hl-todo
-  :config (global-hl-todo-mode))
-
-(use-package define-it
-  :custom (define-it-show-google-translate nil))
-
-(use-package helpful
-  :custom (helpful-max-buffers 3))
-
-(use-package marginalia
-  :config (marginalia-mode))
-
-(use-package which-key
-  :config (which-key-mode)
-  :custom (which-key-sort-order 'which-key-key-order-alpha))
-
-(use-package vertico
-  :config (vertico-mode)
-  :custom (vertico-resize t))
-
-(use-package projectile
-  :config (projectile-mode +1)
-  :custom (projectile-project-search-path '("~/git/"))
-  :hook   (projectile-after-switch-project . vc-pull))
-
-(use-package corfu
-  :config (global-corfu-mode)
-  :custom (corfu-auto t))
-
-(use-package orderless
-  :custom
-  (completion-styles '(orderless basic))
-  (orderless-matching-styles '(orderless-flex)))
-
-(use-package tempo
-  :config
-  (tempo-define-template "today"  '((format-time-string "%Y-%m-%d")) "today")
+; Completion Stack
+(use-package vertico              :config (vertico-mode)
+                                  :custom (vertico-resize t))
+(use-package projectile           :config (projectile-mode +1)
+                                  :custom (projectile-project-search-path '("~/git/"))
+                                  :hook   (projectile-after-switch-project . vc-pull))
+(use-package corfu                :config (global-corfu-mode)
+                                  :custom (corfu-auto t))
+(use-package orderless            :custom (completion-styles '(orderless basic)) (orderless-matching-styles '(orderless-flex)))
+(use-package tempo                :config (tempo-define-template "today"  '((format-time-string "%Y-%m-%d")) "today")
   (tempo-define-template "online" '("@online{" p "," n "  author = \"" p "\"," n "  title = \"" p "\"," n "  url = \"" p "\"," n "  date = \"" p "\"," n "}") "online")
   (tempo-define-template "book"   '("@book{" p "," n "  author = \"" p "\"," n "  title = \"" p "\"," n "  year = \"" p "\"," n "  publisher = \"" p "\"," n "}") "book")
   (tempo-define-template "fig"    '("#+CAPTION: " p n "[[./assets/" p "]]" n) "fig")
   (tempo-define-template "sfig"   '("#+CAPTION: " p n "#+ATTR_LATEX: :height 0.1\\textwidth" n "[[./assets/" p "]]" n) "sfig")
   (tempo-define-template "np"     '("#+LATEX:\\newpage" n) "np"))
 
-(use-package consult-git-log-grep
-  :custom (consult-git-log-grep-open-function 'magit-show-commit))
+; Git
+(use-package consult-git-log-grep :custom (consult-git-log-grep-open-function 'magit-show-commit))
+(use-package magit                :custom (magit-slow-confirm nil))
+(use-package git-gutter           :config (global-git-gutter-mode))
 
-(use-package magit
-  :custom (magit-slow-confirm nil))
-
-(use-package git-gutter
-  :config (global-git-gutter-mode))
-
-(use-package markdown-mode
-  :mode "\\.md\\'")
-
-(use-package keepass-mode
-  :mode "\\.kdbx\\'")
-
-(use-package ruff-format
-  :hook (python-mode . ruff-format-on-save-mode))
-
-(use-package flymake-ruff
-  :hook python-mode)
-
-(use-package shfmt
-  :hook (sh-mode . shfmt-on-save-mode)
-  :custom (shfmt-arguments '("-i" "4" "-ci")))
-
-(use-package flymake
-  :hook (sh-mode python-mode))
-
+; Filetype-specific modes
+(use-package markdown-mode :mode   "\\.md\\'")
+(use-package keepass-mode  :mode   "\\.kdbx\\'")
+(use-package ruff-format   :hook   (python-mode . ruff-format-on-save-mode))
+(use-package flymake-ruff  :hook   (python-mode . flymake-mode)
+                                   (python-mode . flymake-ruff-mode))
+(use-package shfmt         :hook   (sh-mode . shfmt-on-save-mode)
+                                   (sh-mode . flymake-mode)
+                           :custom (shfmt-arguments '("-i" "4" "-ci")))
 (use-package paredit)
+(use-package sly-overlay   :custom (inferior-lisp-program "/usr/bin/sbcl")
+                           :hook   (sly-mode . (lambda () (unless (sly-connected-p) (save-excursion (sly))))))
 
-(use-package sly-overlay
-  :custom (inferior-lisp-program "/usr/bin/sbcl")
-  :hook (sly-mode . (lambda () (unless (sly-connected-p) (save-excursion (sly))))))
+; Multimedia
+(use-package pdf-tools            :config (pdf-loader-install t))
+(use-package elfeed               :custom (elfeed-feeds '("https://planet.archlinux.org/rss20.xml"
+                                                          "https://sachachua.com/blog/category/emacs-news/feed/"
+                                                          "https://blog.fefe.de/rss.xml?html")))
+(use-package ytdl                 :custom (ytdl-command "yt-dlp")
+                                          (ytdl-music-folder "~/mu")
+                                          (ytdl-video-folder "~/dl"))
 
-(use-package pdf-tools
-  :config (pdf-loader-install t))
+; Terminals & Aliases
+(use-package vterm                :custom (vterm-always-compile-module t))
+(use-package eshell-toggle        :custom (eshell-history-size 100000))
+(use-package esh-autosuggest      :hook   eshell-mode)
 
-(use-package elfeed
-  :custom
-  (elfeed-feeds '("https://planet.archlinux.org/rss20.xml"
-                  "https://sachachua.com/blog/category/emacs-news/feed/"
-                  "https://blog.fefe.de/rss.xml?html")))
-
-(use-package ytdl
-  :custom
-  (ytdl-command "yt-dlp")
-  (ytdl-music-folder "~/mu")
-  (ytdl-video-folder "~/dl"))
-
-(use-package vterm
-  :custom (vterm-always-compile-module t))
-
-(use-package eshell-toggle
-  :custom (eshell-history-size 100000))
-
-(use-package esh-autosuggest
-  :hook eshell-mode)
-
-(use-package evil
-  :init (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1)
-  (define-key evil-motion-state-map "," nil)
-  :custom
-  (evil-undo-system 'undo-redo))
-
-(use-package evil-goggles
-  :config (evil-goggles-mode))
-
-(use-package evil-vimish-fold
-  :config (global-evil-vimish-fold-mode))
-
-(use-package evil-collection
-  :config (evil-collection-init))
-
+; Key Bindigns
+(use-package evil                 :init   (setq evil-want-keybinding nil)
+                                  :config (evil-mode 1)
+				          (define-key evil-motion-state-map "," nil)
+                                  :custom (evil-undo-system 'undo-redo))
+(use-package evil-goggles         :config (evil-goggles-mode))
+(use-package evil-vimish-fold     :config (global-evil-vimish-fold-mode))
+(use-package evil-collection      :config (evil-collection-init))
 (use-package general
   :config
   (general-evil-setup)
@@ -331,6 +242,7 @@
     "r" '(consult-recent-file :which-key "open recent")
     "R" '(launch :which-key "launcher")
     "s" '(flameshot :which-key "screenshot")
+    ; TODO t for toggling the space map between lisp and org
     "u" '(consult-theme :which-key "change theme")
     "v" '(eval-last-sexp :which-key "(emacs) eval")
     "w" `(,my-window-map :which-key "Windows")
